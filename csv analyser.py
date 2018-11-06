6a29ec4acb5fafce4d0d11a0508a3238d3ff7acf
Huang Zhibo <30903626+storyinvisible@users.noreply.github.com>
2018-11-06 21:17:58 +0800

    Update functions with header

--- csv analyser.py
+++ csv analyser.py
@@ -4,11 +4,43 @@ row = 1
 ip=""
 index =0
 write_data=[]
+col_header1= ["heartbleed",
+              "CCS",
+              "ticketbleed",
+              "ROBOT",
+              "secure_renego",
+              "secure_client_renego",
+              "CRIME_TLS,BREACH,POODLE_SSL",
+              "fallback_SCSV",
+              "SWEET32",
+              "FREAK",
+              "DROWN",
+              "DROWN",
+              "LOGJAM",
+              "LOGJAM-common_primes",
+              "BEAST_CBC_TLS1",
+              "BEAST","LUCKY13",
+              "RC4"
+]
+col_header2 =["ok","LOW", "MEDIUM","HIGH","Comment"]
+lin1 = ["IP"]
+lin2=["IP"]
+for l in col_header1:
+    for i in range(0,6):
+        lin1.append(l)
+    for g in col_header2:
+        lin2.append(g)
+
+print(lin1)
+print(lin2)
+with open("report.csv", "w") as report:
+    reader=csv.writer(report, dialect="excel")
+    reader.writerow(lin1)
+    reader.writerow(lin2)
 def info_b(info):
     r_msg= [0,0,0,0]
     if info == "OK":
-        r_msg = [1,0,0,0]#[OK, Low, Medium, High]
-        
+        r_msg = [1,0,0,0]#[OK, Low, Mediul
     elif info =="LOW":
         r_msg =[0,1,0,0]
     elif info =="MEDIUM":
@@ -26,6 +58,7 @@ with open("ip1.csv", "r") as ip1:
 something = []
 for i in range(len(data)):
     row_data = data[i]
+    print("i = %d",format(i))
     if i!=0:
         
         something = row_data
@@ -38,16 +71,22 @@ for i in range(len(data)):
                 write_data= [ip] + write_data
                 print(write_data)
         
-        info= info_b(row_data[3])
-        for x in info:
-            write_data.append(x)
-        write_data.append(row_data[4])
+            info= info_b(row_data[3])
+            for x in info:
+                write_data.append(x)
+            write_data.append(row_data[4])
 
-        print("write_data" + str(len(write_data)))
+            print("write_data" + str(len(write_data)))
+            if i == len(data)-1:
+                with open("report.csv", "a") as report:
+                    writer=csv.writer(report, dialect="excel")
+                    writer.writerow(write_data)
    
     
-        if (row_data[0]=="service"):
+        elif(row_data[0]=="service"):
             with open("report.csv", "a") as report:
                 writer=csv.writer(report, dialect="excel")
                 writer.writerow(write_data)
-            del write_data[:]                     
+            del write_data[:]
+            print("initial")
+            print(write_data)
