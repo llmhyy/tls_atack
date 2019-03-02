f77797c527e1e7f6af193f3b6ab938d4cc0104f7
Yilong94 <yilong_tan@mymail.sutd.edu.sg>
2019-03-02 13:23:02 +0800

    Resolve string formatting error

--- rnn_model/utils_plot.py
+++ rnn_model/utils_plot.py
@@ -86,7 +86,7 @@ def plot_distribution(final_acc, overall_mean_acc, feature_filename, save_dir, s
     plt.xlabel('Traffic #')
     plt.ylabel('Mean Cosine Similarity')
     plt.axhline(y=round(overall_mean_acc,5), color='r', linestyle='-')
-    plt.text(0, round(overall_mean_acc,5), '{:.3f}'.format(overall_mean_acc), color='r', horizontalalignment='right', verticalalignment='center', transform=ax.transAxes)
+    plt.text(0, round(overall_mean_acc,5), '{:.5f}'.format(overall_mean_acc), color='r', horizontalalignment='right', verticalalignment='center', transform=ax.transAxes)
     plt.savefig(os.path.join(save_dir, 'dist_true_({})'.format(feature_filename)))
     if show:
         plt.show()
